74e95498bdb2a522852c50e2967221145d1f01d6
quizcanners <quizcanners@gmail.com>
2019-02-04 19:12:58 +0300

    Inspection

--- Visual_Layer/Scripts/Nodes_Inspector.cs
+++ Visual_Layer/Scripts/Nodes_Inspector.cs
@@ -9,9 +9,9 @@ using UnityEditor;
 namespace NodeNotes_Visual
 {
     [CustomEditor(typeof(Nodes_PEGI))]
-    public class Nodes_PEGIEditor : PEGI_Editor<Nodes_PEGI> {  }
+    public class Nodes_PEGIEditor : PEGI_Inspector<Nodes_PEGI> {  }
 
     [CustomEditor(typeof(NodeCircleController))]
-    public class NodeCircleControllerEditor : PEGI_Editor<NodeCircleController>  {  }
+    public class NodeCircleControllerEditor : PEGI_Inspector<NodeCircleController>  {  }
 }
 #endif
